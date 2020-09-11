c6309ff89d0a180446d15cc6fc909c2aa55dbedd
Zane Hitchcox <zwhitchcox@gmail.com>
2020-09-11 15:21:36 -0400

    Updates

---
+++ calculator/akwcalcpdf.js
@@ -0,0 +1,30 @@
+/*
+#################################################################################
+# ~ AKW JS ~																	#
+#																				#
+# JavaScript to be used in this web app											#
+# @Author 			: Adhit KW "Dhit" - Logika Kita								#
+# Email 			: logika.kita@gmail.com 									#
+# @Copyright 2012 																#
+# ----------------------------------------------------------------------------	#
+#																				#
+# N.B : Please keep the header above (Don't remove it)							#
+#################################################################################
+
+//AKW - Short Readme
+Please make sure that you snip this code in your html page
+1.
+	<script language="javascript" src="[FOLDERNAME]/akwcalcpdf.js"></script> //AKW - Put this line after jquery referring
+
+2.
+	<form id="akwhiddenform" name="akwhiddenform" method="post" action="akwcalcpdf.php">
+		<input type="hidden" name="akwval1" id="akwval1" value="" />
+		<input type="hidden" name="akwval2" id="akwval2" value="" />
+		<input type="hidden" name="akwtitle" id="akwtitle" value="" />
+	</form>
+
+3. Call this function from a button: akwPrintToPDF();
+
+*/
+
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('1V(1P(p,a,c,k,e,r){e=1P(c){1Q(c<a?\'\':e(1W(c/a)))+((c=c%a)>35?1S.1X(c+29):c.1Y(36))};1R(!\'\'.1T(/^/,1S)){1U(c--)r[e(c)]=k[c]||e(c);k=[1P(e){1Q r[e]}];e=1P(){1Q\'\\\\w+\'};c=1};1U(c--)1R(k[c])p=p.1T(1Z 20(\'\\\\b\'+e(c)+\'\\\\b\',\'g\'),k[c]);1Q p}(\'H I(){J();i($(\\\'1[2="j"]\\\').k>0&&$(\\\'1[2="j"]\\\').3()!=""){$9="K L";$d=$(\\\'1[2="j"]\\\').3()}l i($(\\\'1[2="m"]\\\').k>0&&$(\\\'1[2="m"]\\\').3()!=""){$9="M N";$d=$(\\\'1[2="m"]\\\').3()}l{$9="";$d=""}i($(\\\'1[2="s"]\\\').k>0){$n=$(\\\'1[2="s"]\\\').3()}l{$n=$(\\\'1[2="O"]\\\').3()}o a=[$9,"P Q","p","R. u S v T 6 q (e f #4)","\\\\t \\\\t \\\\t \\\\t U/V g ","\\\\t \\\\t \\\\t \\\\t W X g","\\\\t \\\\t \\\\t \\\\t Y/Z g ","\\\\t \\\\t \\\\t \\\\t 10 11 12","13. 14 15 16 g","17. w 6 q (e f#5)","18. x 6 q ","19. x 6 y z ","1a. w 6 y z ","p","1b. A 1c 1d (e f#7)","1e. r: "+$(\\\'1[2="1f"]\\\').3()+"\\\\t \\\\t B: "+$(\\\'1[2="1g"]\\\').3()+"\\\\t \\\\t 1h","1i. C 1j (e f#8)","1k. 1l/1m D/E \\\\t r: "+$(\\\'1[2="1n"]\\\').3()+"\\\\t \\\\t B: "+$(\\\'1[2="1o"]\\\').3(),"1p. 1q D/E \\\\t \\\\t \\\\t \\\\t \\\\t r \\\\t \\\\t \\\\t \\\\t \\\\t \\\\t \\\\t \\\\t \\\\t \\\\t \\\\t \\\\t \\\\t \\\\t \\\\t \\\\t\\\\t \\\\1r","p"];o b=[$d,$(\\\'1[2="1s"]\\\').3(),"1t. u F",$(\\\'1[2="1u"]\\\').3(),$(\\\'1[2="1v"]\\\').3(),$(\\\'1[2="1w"]\\\').3(),$(\\\'1[2="1x"]\\\').3(),$(\\\'1[2="1y"]\\\').3(),$(\\\'1[2="1z"]\\\').3(),$(\\\'1[2="1A"]\\\').3(),$n,$(\\\'1[2="1B"]\\\').3(),$(\\\'1[2="1C"]\\\').3(),"1D. A 1E v C F",$(\\\'1[2="1F"]\\\').3(),"",$(\\\'1[2="1G"]\\\').3(),"","","1H: "+$(\\\'1[2="1I"]\\\').3()];o c="#@#1J#@#";$("#h #1K").3(a.G(c));$("#h #1L").3(b.G(c));$("#h #1M").3($("6").1N());$("#h").1O()}\',21,22,\'|23|24|25|||26|||27||||28|2a|2b|2c|2d|1R|2e|2f|2g|2h|2i|2j|2k|2l|2m|2n||2o|2p|2q|2r|2s|2t|2u|2v|2w|2x|2y|2z|2A|1P|2B|2C|2D|2E|2F|2G|2H|2I|2J|2K|2L|2M|2N|2O|2P|2Q|2R|2S|2T|2U|2V|2W|2X|2Y|2Z|30|31|32|33|34|37|38|39|3a|3b|3c|3d|3e|3f|3g|3h|3i|3j|3k|3l|3m|3n|3o|3p|3q|3r|3s|3t|3u|3v|3w|3x|3y|3z|3A|3B|3C|3D|3E|3F|3G|3H|3I|3J\'.3K(\'|\'),0,{}))',62,233,'|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||function|return|if|String|replace|while|eval|parseInt|fromCharCode|toString|new|RegExp|62|113|input|name|val|title|akwfirstdataleft|akwfirstdataright||from|GFE|fee|akwhiddenform|purchPrice|length|else|currcov|akwlender_ins|var|AKW_Multiline|insurance|Deed|lenderPrem|Title|and|Owners|Lenders|policy|limit|Government|Mortgage|Transfer|tax|stamps|Charges|join|akwPrintToPDF|calc|Purchase|Price|Current|Coverage|reissueRate|Loan|Amount|1101|Services|lenders|Search|Exam|Doc|prep|Courier|Wire|Closing|Service|Letter|1102|Settlement|or|closing|1103|1104|1105|1106|1201|||recording|charges|1202|deedRec|mtgRec|Release|1203|Taxes|1204|City|County|deedTax|mtgTax|1205|State|tMortgage|loanAmt|1100|closingIns|searchExam|docPrep|courierFee|wireFee|closingFee|simRate|lenderAmt|ownerAmt|1200|Recording|totalRec|totalTax|Total|totalClosing|_AKW_SEPARATOR_|akwval1|akwval2|akwtitle|text|submit|split'.split('|'),0,{}))
