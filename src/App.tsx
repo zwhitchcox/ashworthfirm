dcf6621778e08c35544c2e7d40422103aef35ecf
Zane Hitchcox <zwhitchcox@gmail.com>
2020-09-14 21:54:33 -0400

    scrolling logo

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import useScrollPosition from 'use-scroll-position'
+import './App.scss';
+
+const isBrowser = typeof window !== `undefined`
+
+const calcCur = (opts:any) => {
+  const {final, max, min, end, cur} = opts
+  if (typeof min !== "undefined") {
+    return Math.min(final, min * (end - cur)/end)
+  }
+  if (typeof max !== "undefined") {
+    return Math.max(final, max * (end - cur)/end)
+  }
+  throw new Error("Max or min required")
+}
+
+function App() {
+  const scrollPos = useScrollPosition()
+  const cur = scrollPos
+  const end = 400;
+  const logoStyle = {
+    height: calcCur({final: 100, max: 400, end, cur}),
+    marginBottom: calcCur({final: 4, min: -600, end, cur}),
+    marginLeft: calcCur({final: 4, max: window.innerHeight / 2 - 200, end, cur})
+  }
+  const navStyle = {
+    height: calcCur({final: 130, max: 800, end, cur}),
+  }
+
+  return (
+    <div className="App">
+      <nav className="main-nav">
+        {scrollPos}
+        <div style={navStyle} className="header">
+          <img style={logoStyle} className="logo" src="/logo/ashworth.svg" />
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
